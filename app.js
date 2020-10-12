abf4d534bd19fd3c1474a5a9c149558a907f2e56
Your Name <you@example.com>
2020-10-12 15:42:05 +0530

    started with puppeteer app

---
+++ app.js
@@ -0,0 +1,23 @@
+const puppeteer = require('puppeteer');
+const conn = require('./connection');
+require('dotenv').config()
+const { uuid } = require('uuidv4');
+const express = require('express');
+const bodyParser = require('body-parser');
+const user = require('./routers/users');
+const puppeteerService = require('./services/puppeteer');
+
+let app = express();
+
+app.use(bodyParser.json({ limit: '50mb' }))
+app.use(bodyParser.urlencoded({
+    limit: '50mb',
+    'extended': 'true'
+}))
+
+app.use('/api', user)
+
+
+var PORT = process.env.PORT || 3000
+app.listen(PORT)
+console.log('Started server on port ' + PORT)
